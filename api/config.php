cd46107641740d5b0db3400f194344cecfb3b778
wilmarvr <wilmarvr@gmail.com>
2025-11-18 19:33:32 +0100

    ..

--- api/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array (
-  'host' => 'localhost',
-  'port' => 3306,
-  'user' => 'vislokaties',
-  'password' => 'V(dWy.W2cAI0Ks3Z',
-  'database' => 'vis_lokaties',
-);
--- install.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Convenience shim so hosts can visit /install.php instead of /api/install.php.
-require __DIR__ . '/api/install.php';
